50831713ed4883e3973ce789f7b7818302e1d8a1
richerfu <southorange0929@foxmail.com>
2024-11-27 20:40:43 +0800

    feat: remove ref logic

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -3,7 +3,7 @@ use openharmony_activity::App;
 use openharmony_activity_derive::activity;
 
 #[activity]
-fn openharmony_app(app: &App) {
+fn openharmony_app(app: App) {
     app.run_loop(|types| {
         hilog_info!(format!("ohos-rs macro: {:?}", types.as_str()).as_str());
         hilog_info!("ohos-rs macro");
