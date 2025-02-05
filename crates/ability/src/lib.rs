4f35a919a9191497b0582851447cecde726de104
richerfu <southorange0929@foxmail.com>
2025-02-05 09:49:25 +0800

    Bump version

--- crates/ability/src/lib.rs
+++ crates/ability/src/lib.rs
@@ -16,16 +16,16 @@ pub use area::*;
 pub use configuration::*;
 pub use draw::*;
 pub use event::*;
+pub use helper::*;
 pub use input::*;
 pub use lifecycle::*;
 pub use memory::*;
 pub use render::*;
 pub use stage::*;
 pub use waker::*;
-pub use helper::*;
 
 // re-export arkui and avoid the need to import it in the lib.rs
+pub use napi_derive_ohos as napi_derive;
+pub use napi_ohos as napi;
 pub use ohos_arkui_binding as arkui;
 pub use ohos_xcomponent_binding as xcomponent;
-pub use napi_ohos as napi;
-pub use napi_derive_ohos as napi_derive;
