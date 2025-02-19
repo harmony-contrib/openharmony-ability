86481bc0d791cd227abaed4de92dde9b1fdb1508
richerfu <southorange0929@foxmail.com>
2025-02-19 14:55:54 +0800

    refactor(derive): add mod for inner method and avoid import napi crate

--- crates/derive/src/lib.rs
+++ crates/derive/src/lib.rs
@@ -9,35 +9,40 @@ pub fn ability(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let arg = &ast.sig.inputs;
 
     let f = quote::quote! {
-        fn #fn_name(#arg) #block
+        pub(crate) fn #fn_name(#arg) #block
 
-        static APP: std::sync::LazyLock<openharmony_ability::OpenHarmonyApp> =
-        std::sync::LazyLock::new(|| openharmony_ability::OpenHarmonyApp::new());
-    
-        thread_local! {
-            pub static ROOT_NODE: std::cell::RefCell<Option<openharmony_ability::arkui::RootNode>> = std::cell::RefCell::new(None);
-        }
-        
-        #[openharmony_ability::napi_derive::napi]
-        pub fn init<'a>(
-            env: &'a openharmony_ability::napi::Env,
-            helper: openharmony_ability::ArkTSHelper,
-        ) -> openharmony_ability::napi::Result<openharmony_ability::ApplicationLifecycle<'a>> {
-            let lifecycle_handle =
-                openharmony_ability::create_lifecycle_handle(env, helper, (*APP).clone())?;
-            #fn_name((*APP).clone());
-            Ok(lifecycle_handle)
-        }
-        
-        #[openharmony_ability::napi_derive::napi]
-        pub fn render<'a>(
-            env: &'a openharmony_ability::napi::Env,
-            slot: openharmony_ability::arkui::ArkUIHandle,
-            callback: openharmony_ability::napi::bindgen_prelude::Function<'a, (), ()>,
-        ) -> openharmony_ability::napi::Result<openharmony_ability::Render<'a>> {
-            let (root, ret) = openharmony_ability::render(env, slot, callback, (*APP).clone())?;
-            ROOT_NODE.replace(Some(root));
-            Ok(ret)
+        mod openharmony_ability_mod {
+            use openharmony_ability::napi as napi_ohos;
+            use crate::*;
+
+            static APP: std::sync::LazyLock<openharmony_ability::OpenHarmonyApp> =
+            std::sync::LazyLock::new(|| openharmony_ability::OpenHarmonyApp::new());
+
+            thread_local! {
+                pub static ROOT_NODE: std::cell::RefCell<Option<openharmony_ability::arkui::RootNode>> = std::cell::RefCell::new(None);
+            }
+
+            #[openharmony_ability::napi_derive::napi]
+            pub fn init<'a>(
+                env: &'a openharmony_ability::napi::Env,
+                helper: openharmony_ability::ArkTSHelper,
+            ) -> openharmony_ability::napi::Result<openharmony_ability::ApplicationLifecycle<'a>> {
+                let lifecycle_handle =
+                    openharmony_ability::create_lifecycle_handle(env, helper, (*APP).clone())?;
+                #fn_name((*APP).clone());
+                Ok(lifecycle_handle)
+            }
+
+            #[openharmony_ability::napi_derive::napi]
+            pub fn render<'a>(
+                env: &'a openharmony_ability::napi::Env,
+                slot: openharmony_ability::arkui::ArkUIHandle,
+                callback: openharmony_ability::napi::bindgen_prelude::Function<'a, (), ()>,
+            ) -> openharmony_ability::napi::Result<openharmony_ability::Render<'a>> {
+                let (root, ret) = openharmony_ability::render(env, slot, callback, (*APP).clone())?;
+                ROOT_NODE.replace(Some(root));
+                Ok(ret)
+            }
         }
     };
 
--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,6 +1,13 @@
+use napi_derive_ohos::napi;
 use ohos_hilog_binding::hilog_info;
 use openharmony_ability::{Event, InputEvent, OpenHarmonyApp};
 use openharmony_ability_derive::ability;
+
+// test add more napi method
+#[napi]
+pub fn handle_change() -> napi_ohos::Result<()> {
+    Ok(())
+}
 
 #[ability]
 fn openharmony_app(app: OpenHarmonyApp) {
