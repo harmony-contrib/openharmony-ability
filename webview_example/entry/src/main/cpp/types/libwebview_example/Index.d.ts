84fc8f683ce3dd02d121d977a52a157bef91b519
richerfu <32590310+richerfu@users.noreply.github.com>
2025-12-24 10:54:53 +0800

    chore(format): allow using oxk to format ets (#41)
    * chore(format): allow using oxk to format ets
    
    * ci: cache cargo install

--- webview_example/entry/src/main/cpp/types/libwebview_example/Index.d.ts
+++ webview_example/entry/src/main/cpp/types/libwebview_example/Index.d.ts
@@ -2,52 +2,52 @@
 /* eslint-disable */
 
 export interface ApplicationLifecycle {
-  environmentCallback: EnvironmentCallback
-  windowStageEventCallback: WindowStageEventCallback
+  environmentCallback: EnvironmentCallback;
+  windowStageEventCallback: WindowStageEventCallback;
 }
 
 export interface ArkTSHelper {
-  exit: (arg: number) => void
-  createWebview: (arg: WebViewInitData) => Object
+  exit: (arg: number) => void;
+  createWebview: (arg: WebViewInitData) => Object;
 }
 
 export interface EnvironmentCallback {
-  onConfigurationUpdated: () => void
-  onMemoryLevel: (arg: number) => void
+  onConfigurationUpdated: () => void;
+  onMemoryLevel: (arg: number) => void;
 }
 
 export interface WebViewComponentEventCallback {
-  onComponentCreated: () => void
-  onComponentDestroyed: () => void
+  onComponentCreated: () => void;
+  onComponentDestroyed: () => void;
 }
 
 export interface WebViewInitData {
-  url?: string
-  id?: string
-  style?: WebViewStyle
+  url?: string;
+  id?: string;
+  style?: WebViewStyle;
 }
 
 export interface WebViewStyle {
-  x?: number | string
-  y?: number | string
+  x?: number | string;
+  y?: number | string;
 }
 
 export interface WindowStageEventCallback {
-  onWindowStageCreate: () => void
-  onWindowStageDestroy: () => void
-  onAbilityCreate: () => void
-  onAbilityDestroy: () => void
-  onAbilitySaveState: () => void
-  onAbilityRestoreState: () => void
-  onWindowStageEvent: (arg: number) => void
-  onWindowSizeChange: (arg: object) => void
-  onWindowRectChange: (arg: object) => void
+  onWindowStageCreate: () => void;
+  onWindowStageDestroy: () => void;
+  onAbilityCreate: () => void;
+  onAbilityDestroy: () => void;
+  onAbilitySaveState: () => void;
+  onAbilityRestoreState: () => void;
+  onWindowStageEvent: (arg: number) => void;
+  onWindowSizeChange: (arg: object) => void;
+  onWindowRectChange: (arg: object) => void;
 }
 
-export declare function handleChange(): void
+export declare function handleChange(): void;
 
-export declare function init(): ApplicationLifecycle
+export declare function init(): ApplicationLifecycle;
 
-export declare function webviewRender(helper: ArkTSHelper): WebViewComponentEventCallback
-export declare function setBackgroundColor(color: string): void
-export declare function setVisible(visible: boolean): void
+export declare function webviewRender(helper: ArkTSHelper): WebViewComponentEventCallback;
+export declare function setBackgroundColor(color: string): void;
+export declare function setVisible(visible: boolean): void;
